39737f49323cbcd2a445028384372f5a9b1944ba
Bertrik Sikken <bertrik@sikken.nl>
2025-01-01 22:15:07 +0100

    Add specification link to IPacketBrokerRestApi

--- packetbroker-utils/src/main/java/nl/bertriksikken/packetbroker/IPacketBrokerRestApi.java
+++ packetbroker-utils/src/main/java/nl/bertriksikken/packetbroker/IPacketBrokerRestApi.java
@@ -5,9 +5,12 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
+/**
+ * See <a href="https://packetbroker.net/getting-started/api/">Packet broker API</a>
+ */
 public interface IPacketBrokerRestApi {
 
     @GET("/api/v2/gateways")
-    public Call<List<GatewayInfo>> getAllGateways();
+    Call<List<GatewayInfo>> getAllGateways();
     
 }
